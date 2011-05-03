03ed20b0dc1898fc50e8dc1257546c7722e174bf
liupeng <laupeng11@gmail.com>
2011-05-03 18:24:44 +0800

    pager

---
+++ src/com/ppstream/mt/utils/SessionListener.java
@@ -0,0 +1,35 @@
+package com.ppstream.mt.utils;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class SessionListener
+ *
+ */
+@WebListener
+public class SessionListener implements HttpSessionListener {
+
+    /**
+     * Default constructor. 
+     */
+    public SessionListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+	
+}
